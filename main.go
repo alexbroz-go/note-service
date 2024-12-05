4ef53b8ace702a36a33fd6474af7836ae839e923
alexbroz-go <alex.aksenov.work675@gmail.com>
2024-12-05 14:24:34 +0300

    Первый сервер

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Record video", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	var router = gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:9090")
 }
